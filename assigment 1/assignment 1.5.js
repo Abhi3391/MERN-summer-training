0047f5ea57ede1758220a6109717148bca888ff0
Abhishek Sharma <99042525+Abhi3391@users.noreply.github.com>
2023-07-01 19:44:05 +0530

    assignment 1.5.js

--- assigment 1/assignment 1.5.js
+++ assigment 1/assignment 1.5.js
@@ -1,6 +1,6 @@
-5.WAP to find out if 1st January will be a Sunday between 2000 and 2030
+// 5.WAP to find out if 1st January will be a Sunday between 2000 and 2030
 
-code:
+// code:
 
 for(var i=2000;i<2031;i++){
     var date=new Date(i,0,1,1,1,1,1);
